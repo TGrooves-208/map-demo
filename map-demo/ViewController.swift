760f31b871313f91f7e307fd257c8e097c51f9cd
TGrooves-208 <tgrooves.208@gmail.com>
2016-01-05 23:15:46 -0700

    Dawson's Coffee House added for the location

--- map-demo/ViewController.swift
+++ map-demo/ViewController.swift
@@ -1,18 +1,39 @@
 //
 //  ViewController.swift
 //  map-demo
-//
+//  Dawson's Coffee Roaster's is the set coordinates for the location
 //  Created by Gil Aguilar on 1/5/16.
 //  Copyright © 2016 toplevel. All rights reserved.
-//
+//  A quick way of locking in some coordinates for a specific location
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //43.607745, -116.212378
+        
+        //Using let as opposed to var due to the coordinates being a constant
+        let latitude:CLLocationDegrees = 43.607745
+        
+        let longitude:CLLocationDegrees = -116.212378
+        
+        let latDelta:CLLocationDegrees = 0.01
+        
+        let lonDelta:CLLocationDegrees = 0.01
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region, animated: false)
+        
     }
 
     override func didReceiveMemoryWarning() {
